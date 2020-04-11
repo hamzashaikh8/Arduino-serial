ca3351f36cb7cb69bdeaf65685621d5f44ce4986
hamzashaikh8 <57274894+hamzashaikh8@users.noreply.github.com>
2020-04-11 12:19:01 -0400

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,30 +1,30 @@
 import serial
 from serial import Serial
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+global ser
+try:
+  ser = serial.Serial('COM8', 9600)
+except:
+  print("Error, Serial Connection failed! Please try again!")
+
 
 @app.route('/', methods=['POST', 'GET'])
-def hello_world():
+def hello():
   # we will try to establish a serial connection with the arduino
-  global ser
-  try:
-    ser = serial.Serial('COM8', 9600)
-  except:
-    print("Error, Serial Connection failed! Please try again!")
   if request.method == "POST":  # POST method associated with ON in main.html
     ser.write('c'.encode())
   elif request.method == "GET":
-    ser.write('x'.encode()) #random input to trigger pin 13 to LOW
+    ser.write('x'.encode())  # random input to trigger pin 13 to LOW
   else:
-    pass #nothing happens
+    pass  # nothing happens
   return render_template('main.html')
+
 
 if __name__ == '__main__':
   app.run()
-
-
 
 
 
